e09b8d114005d08cd48a4a85158444c84f50ca2a
ta-kiyama <ta.kiyama@eimee.co.jp>
2019-05-22 19:12:38 +0900

    不要な参照の削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 import Helmet from "@/components/helmet"
-import * as style from "@/styles/index.module.scss";
 import TopBackground from "@/components/topBackground"
 
-const IndexPage = ({data}) => (
+const IndexPage = () => (
   <>
     <Helmet />
     <TopBackground />

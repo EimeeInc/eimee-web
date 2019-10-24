51bf2927a39ff2377c129fea79b652cb92e5adff
yo_ando <yo.ando@eimee.co.jp>
2019-10-24 18:05:35 +0900

    プレスリリースを追加

--- src/components/Organism/NewsPR.tsx
+++ src/components/Organism/NewsPR.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import styled from "@emotion/styled";
 import PRBlock from "@/components/Molecule/PRBlock";
 
-const StyledLine = styled.p``;
+const StyledLink = styled.a``;
 
 const Wrapper = styled.div`
   margin: 60px 0;
@@ -17,11 +17,14 @@ const HorizontalLine = styled.hr`
 const NewsPR = () => (
   <Wrapper>
     <PRBlock>
-      <PRBlock.Header dateTime={new Date("2012-10-01T00:00:00.000+09:00")} />
+      <PRBlock.Header dateTime={new Date("2019-10-07T00:00:00.000+09:00")} />
       <PRBlock.Body>
-        <StyledLine>
-          ダブルアールシステムズ株式会社を愛知県名古屋市に設立
-        </StyledLine>
+        <StyledLink
+          target="_blank"
+          href="https://prtimes.jp/main/html/rd/p/000000002.000049176.html"
+        >
+          IoTや電子工作の知識共有サービス「elchika（エルチカ）」をリリース！電子工作を趣味とする人のためのナレッジ投稿サイト
+        </StyledLink>
       </PRBlock.Body>
     </PRBlock>
     <HorizontalLine />

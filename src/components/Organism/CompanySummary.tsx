ce83affa064f0b787d5344cdce6638b94ef385e9
yo-ando <yo.ando@eimee.co.jp>
2020-05-01 11:16:47 +0900

    住所を変更した

--- src/components/Organism/CompanySummary.tsx
+++ src/components/Organism/CompanySummary.tsx
@@ -24,7 +24,8 @@ const CompanySummary = () => (
       <DefinitionBlock.Body>
         <StyledLine>本社</StyledLine>
         <StyledLine>
-          &#12306;135-0064 東京都江東区青海2-7-4 the SOHO 401
+          &#12306;105-0004 東京都港区新橋1-12-9 A-PLACE新橋駅前
+          ビジネスエアポート新橋
         </StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>

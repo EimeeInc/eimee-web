878d9bbd91e14a2c780334efee12982b831242cf
i-ohira <i.ohira@eimee.co.jp>
2020-05-22 14:26:52 +0900

    元々あったOfficeWorkStyleを使用する修正に変えた

--- src/components/Organism/Workstyle.tsx
+++ src/components/Organism/Workstyle.tsx
@@ -1,151 +1,117 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import Link from "@/components/Atom/Link";
-import media from "@/util/breakpoint";
+import FadeInEnteringCard from "@/components/Atom/FadeInEnteringCard";
+import PictureBlock from "@/components/Molecule/PictureBlock";
 
-const Wrapper = styled.ul`
-  width: 100%;
+const StyledFadeInEnteringCard = styled(FadeInEnteringCard)`
+  margin-top: 60px;
   margin-bottom: 60px;
-
-  ${media.lessThan("lg")`
-    padding: 0 3vw;
-  `}
 `;
-const List = styled.li`
-  width: 100%;
-
-  &:nth-of-type(n + 2) {
-    margin-top: 40px;
-  }
-`;
-const Order = styled.ol`
-  width: 100%;
-  margin-top: 10px;
-  padding: 0 15px;
-  counter-reset: item;
-
-  ${media.lessThan("sm")`
-    padding: 0;
-  `}
-
-  & > li {
-    width: 100%;
-    padding-left: 30px;
-    position: relative;
-    font-size: 1.6rem;
-    line-height: 2.2rem;
-    text-align: left;
-
-    ${media.lessThan("lg")`
-      font-size: 1.4rem;
-      line-height: 2rem;
-    `}
-
-    &:nth-of-type(n + 2) {
-      margin-top: 10px;
-    }
-
-    &:before {
-      position: absolute;
-      top: 0;
-      left: 0;
-      letter-spacing: 0.2rem;
-      counter-increment: item;
-      content: "(" counter(item) ")";
-    }
-  }
-`;
-const Heading = styled.h2`
-  width: 100%;
-  margin-bottom: 15px;
-  padding-bottom: 5px;
-  font-size: 2.2rem;
-  font-weight: bold;
-  line-height: 2.2rem;
-  text-align: left;
-  border-bottom: solid 1px #e3e3e3;
-`;
-const SubTitle = styled.h3`
-  width: 100%;
-  margin-top: 20px;
-  font-size: 1.8rem;
-  font-weight: bold;
-  line-height: 2.2rem;
-  text-align: left;
-`;
-const Comment = styled.p`
-  width: 100%;
-  font-size: 1.6rem;
-  line-height: 2.2rem;
-  text-align: left;
-
-  ${media.lessThan("lg")`
-    font-size: 1.4rem;
-    line-height: 2rem;
-  `}
-
-  &:nth-of-type(n + 2) {
-    margin-top: 10px;
-  }
-`;
-
-const StyledLink = styled(Link)``;
 
 const Works = () => (
   <>
-    <Wrapper>
-      <List>
-        <Heading>事務所案内</Heading>
-        <Comment>
-          新橋駅から徒歩1分というアクセスの良さ！コワーキングスペースを利用することが可能です。
-        </Comment>
-      </List>
-      <List>
-        <Heading>便利な制度</Heading>
-        <Order>
-          <List>
-            時差出勤制度
-            <Comment>8～10時までの間で自由な時間に出社できます。</Comment>
-          </List>
-          <List>
-            年次有給休暇前借制度
-            <Comment>入社後すぐに3日分の年次有給休暇を前借出来ます。</Comment>
-          </List>
-          <List>
-            1時間単位の年次有給休暇取得制度
-            <Comment>
-              年間40時間を上限として1時間単位での年次有給休暇取得が可能です。
-            </Comment>
-          </List>
-        </Order>
-      </List>
-      <List>
-        <Heading>テレワーク制度</Heading>
-        <Comment>
-          オフィスや自宅など働く場所を選択できます。2020年2月末（緊急事態宣言が行われる前）に社員の健康を考えテレワークに切り替えました。
-          テレワークの強要は本意ではなく、コロナウイルスが落ち着いたら見直しを行います。
-        </Comment>
-      </List>
-      <List>
-        <Heading>昼食補助</Heading>
-        <Comment>
-          チケットレストランを導入しており、業務中に昼休憩を取得する社員は在宅勤務・事務所勤務問わず昼食の補助が取得できます。
-          毎月7500円分がカードにチャージされてたくさんのお店で利用することが出来ます。
-        </Comment>
-      </List>
-      <List>
-        <Heading>ボードゲーム</Heading>
-        <Comment>
-          定期的に業務後に社員でボードゲームを開催しています。現在はコロナウイルスの影響で3密を避けるため休部中です。
-        </Comment>
-      </List>
-      <List>
-        <Heading>多様な働き方</Heading>
-        <Comment>
-          育児や介護のための短時間での正社員や、週の半分を別の企業で働くなど様々な働き方を応援しています。
-        </Comment>
-      </List>
-    </Wrapper>
+    <StyledFadeInEnteringCard>
+      <PictureBlock>
+        <PictureBlock.Header>新橋駅から徒歩1分のアクセス</PictureBlock.Header>
+        <PictureBlock.Body>
+          弊社は現在、新橋に事務所を構えています。
+          JR新橋駅の徒歩1分圏内にあるため急な悪天候にも左右されることもなく通勤が可能です。
+          事務所で業務を行うほか、施設内には落ち着いた雰囲気のコワーキングスペースもあり、それらを利用することも可能です。
+        </PictureBlock.Body>
+        <PictureBlock.Image src="/assets/img/ph_office_lobby.jpg" alt="lobby" />
+      </PictureBlock>
+    </StyledFadeInEnteringCard>
+    <StyledFadeInEnteringCard>
+      <PictureBlock>
+        <PictureBlock.Header>時差出勤制度</PictureBlock.Header>
+        <PictureBlock.Body>
+          エイミーでは社員の生活スタイルに合わせた働き方を叶える為の制度として「時差出勤制度」を導入しています。
+          出勤時間を朝8時から10時までの間で自由に選択することができ、出勤時間によってが退勤時間が17時から19時の間で変わる為、その日の都合や生活のリズムに合わせた柔軟な働き方を可能にしています。
+          朝や業務後に時間が多く取れるようにする社員も多く、プライベートの用事や家事に時間を充てるなど使い方は様々です。
+        </PictureBlock.Body>
+        <PictureBlock.Image
+          src="/assets/img/ph_office_sweets.jpg"
+          alt="sweets"
+        />
+      </PictureBlock>
+    </StyledFadeInEnteringCard>
+    <StyledFadeInEnteringCard>
+      <PictureBlock>
+        <PictureBlock.Header>年次有給休暇前借制度</PictureBlock.Header>
+        <PictureBlock.Body>
+          新しく入社された方が安心して働けるように「年次有給休暇前借制度」を導入しています。
+          入社後すぐ3日分の年次有給休暇の取得が可能なため、急な用事などにも柔軟に対応が可能です。
+        </PictureBlock.Body>
+        <PictureBlock.Image src="/assets/img/ph_office_bar.jpg" alt="bar" />
+      </PictureBlock>
+    </StyledFadeInEnteringCard>
+    <StyledFadeInEnteringCard>
+      <PictureBlock>
+        <PictureBlock.Header>
+          1時間単位の年次有給休暇取得制度
+        </PictureBlock.Header>
+        <PictureBlock.Body>
+          社員のより柔軟な働き方が可能になるように「1時間単位の年次有給休暇取得制度」を導入しています。
+          年間累計40時間を上限として1時間単位での有給休暇の取得が可能で、朝や夕方に用事がある為
+          一時間だけお休みしたい時や昼に2時間だけ中抜けするなど、時間を有効活用して働くことができます。
+        </PictureBlock.Body>
+        <PictureBlock.Image src="/assets/img/ph_office_gym.jpg" alt="gym" />
+      </PictureBlock>
+    </StyledFadeInEnteringCard>
+    <StyledFadeInEnteringCard>
+      <PictureBlock>
+        <PictureBlock.Header>テレワーク制度</PictureBlock.Header>
+        <PictureBlock.Body>
+          社員の健康や事情に合わせて勤務地を選択できる「テレワーク制度」を導入しています。
+          自宅で業務を行える為、育児や介護をしながら働くことも可能です。
+          2020年5月時点でほとんどの社員がテレワーク制度を利用しており、それぞれに合った環境での働き方を可能にしています。
+        </PictureBlock.Body>
+        <PictureBlock.Image
+          src="/assets/img/ph_office_view.jpg"
+          alt="ocean view"
+        />
+      </PictureBlock>
+    </StyledFadeInEnteringCard>
+    <StyledFadeInEnteringCard>
+      <PictureBlock>
+        <PictureBlock.Header>昼食補助</PictureBlock.Header>
+        <PictureBlock.Body>
+          社員の健康と食生活を支えるために昼食補助としてコンビニやレストランで利用可能な『チケットレストラン』を導入しています。
+          業務中に昼休憩を取得する社員は在宅勤務・事務所勤務問わず利用可能で1人暮らしや家事に手が回りにくい社員にも重宝されています
+        </PictureBlock.Body>
+        <PictureBlock.Image
+          src="/assets/img/ph_office_rooftop.jpg"
+          alt="rooftop"
+        />
+      </PictureBlock>
+    </StyledFadeInEnteringCard>
+    <StyledFadeInEnteringCard>
+      <PictureBlock>
+        <PictureBlock.Header>ボードゲーム</PictureBlock.Header>
+        <PictureBlock.Body>
+          社内同士の交流や論理的思考を養うことを目的として、ボードゲーム倶楽部を定期的に開催しております。
+          ゲームの種類は様々で社員からのリクエストで新たにゲームを購入したり、個人で持ち寄って遊んだりとバラエティーに富んでいます。
+          また、社内外問わず参加を受け付けており、純粋にボードゲームを楽しむ一方でエイミーの持つ雰囲気を直接感じ取って貰うこともできます。
+        </PictureBlock.Body>
+        <PictureBlock.Image
+          src="/assets/img/ph_office_rooftop.jpg"
+          alt="rooftop"
+        />
+      </PictureBlock>
+    </StyledFadeInEnteringCard>
+    <StyledFadeInEnteringCard>
+      <PictureBlock>
+        <PictureBlock.Header>多様な働き方</PictureBlock.Header>
+        <PictureBlock.Body>
+          社員の多様な働き方を応援する一環として副業や時短勤務などの相談をすることも可能です。
+          育児や介護のために短時間の勤務をしたり、週の半分を別の企業で働くなど様々な働き方に対し柔軟に対応できる体制を築いています。
+        </PictureBlock.Body>
+        <PictureBlock.Image
+          src="/assets/img/ph_office_rooftop.jpg"
+          alt="rooftop"
+        />
+      </PictureBlock>
+    </StyledFadeInEnteringCard>
   </>
 );
 

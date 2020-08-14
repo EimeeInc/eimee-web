f8cca0c0a50c70b55ed55ee9d15a0e93275f5a8d
i-ohira <i.ohira@eimee.co.jp>
2020-08-14 09:40:04 +0900

    「システムエンジニア」の作成

--- src/components/Organism/RecruitSystemEngineerSummary.tsx
+++ src/components/Organism/RecruitSystemEngineerSummary.tsx
@@ -5,174 +5,345 @@ import DefinitionCard from "@/components/Atom/DefinitionCard";
 
 const StyledLine = styled.p``;
 
-const RecruitCareerSummary = () => (
+const RecruitSystemEngineerSummary = () => (
   <DefinitionCard>
     <DefinitionBlock>
-      <DefinitionBlock.Header>職種</DefinitionBlock.Header>
+      <DefinitionBlock.Header>募集背景</DefinitionBlock.Header>
       <DefinitionBlock.Body>
         <StyledLine>
-          ・プレイングマネージャー（プレイングマネージャー候補）
+          エイミー株式会社（以下エイミー）では、2020年10月7日に電子工作のナレッジ共有サイト『elchika』を開設いたしました。
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          elchikaが目指している市場においての役割は、「個人、企業問わずモノづくりの知識を共有し、交流が生まれ、イノベーションが起きる」そんなプラットフォームです。
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          しかしながら、現在はまだその役割を果たせていないのが実情です。
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>その理由の一つに『人材』の課題が挙げられます。</StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          そのため、今後elchikaが市場で役割を担うプロダクトに成長するには、開発チームの強化が必要になると考え、この度システムエンジニアを募集することとなりました。
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          要件定義～試験、運用までの一連の開発業務と、コードレビューや開発環境の最適化など、開発チームを牽引する役割をお任せします。
+        </StyledLine>
+        <StyledLine>
+          <br />
         </StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
-      <DefinitionBlock.Header>勤務地</DefinitionBlock.Header>
+      <DefinitionBlock.Header>募集にあたって</DefinitionBlock.Header>
       <DefinitionBlock.Body>
-        <StyledLine>本社</StyledLine>
         <StyledLine>
-          &#12306;105-0004 東京都港区新橋1-12-9 A-PLACE新橋駅前 7F
+          今回募集のシステムエンジニアは、開発リーダー経験をお持ちの方、若しくは職務上リーダーでは無かったものの後輩の技術指導やコードレビューなど、開発力や品質の向上に務めてこられた方に、開発部の技術の牽引をお任せしたいと考えております。
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          今後のキャリア形成も「プレイヤーとして」または「PMとして」など、ご自身のキャリアビジョンやスキルに応じて相談可能です。
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          今まで培ってきた知識や経験を活かして活躍したい方、そして開発環境構築や後輩の育成などに興味のある方には大きなやりがいが感じられるポジションとなります。
+        </StyledLine>
+        <StyledLine>
+          <br />
         </StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
-      <DefinitionBlock.Header>仕事内容</DefinitionBlock.Header>
+      <DefinitionBlock.Header>MUST要件</DefinitionBlock.Header>
       <DefinitionBlock.Body>
         <StyledLine>
-          Webサイトのシステム開発・運用・保守・企画、進捗などの管理業務、開発部の取りまとめや、他部署、代表との調整などに係わる業務を担当して頂きます。
+          ・PG、SE等での開発経験（後述する開発環境下（主に使われている技術）のいずれかでの経験を有する）
         </StyledLine>
-        <br />
-        <StyledLine>「具体的には」</StyledLine>
-        <br />
+        <StyledLine>・各部署との調整、連携を積極的に行える方</StyledLine>
+      </DefinitionBlock.Body>
+    </DefinitionBlock>
+    <DefinitionBlock>
+      <DefinitionBlock.Header>WANT要件</DefinitionBlock.Header>
+      <DefinitionBlock.Body>
+        <StyledLine>・メンバー管理経験</StyledLine>
+        <StyledLine>・スケジュール管理経験</StyledLine>
         <StyledLine>
-          <b>プレイヤーとして</b>
-        </StyledLine>
-        <StyledLine>
-          新規、既存問わず、サービスの企画、設計、開発、運用、保守に係わる業務
-        </StyledLine>
-        <StyledLine>
-          新サービスの企画・立案・コンテンツ作成・システム開発に係わる業務
-        </StyledLine>
-        <br />
-        <StyledLine>
-          <b>マネージャーとして</b>
-        </StyledLine>
-        <StyledLine>
-          品質保証、進捗管理などの開発に責任を持つだけではなく、長期的な目線での、部下の教育・育成、目標設定（評価）に係わる業務
+          ※
+          趣味の範囲でも電子工作を作るのが好き、若しくはIoT分野に興味がある方には、よりやりがいを感じることができる環境です。
         </StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
-      <DefinitionBlock.Header>応募資格</DefinitionBlock.Header>
+      <DefinitionBlock.Header>
+        開発環境（主に使われている技術）
+      </DefinitionBlock.Header>
       <DefinitionBlock.Body>
         <StyledLine>
-          <b>MUST（SE、PLとして）</b>
+          ・<b>言語</b>: JavaScript(ESNext), TypeScript
         </StyledLine>
         <StyledLine>
-          ・企業理念である『みんなが輝く多様な未来を創る』に共感でき、一緒に実現できる方
+          ・<b>フレームワーク</b>: Node.js, Vue.js
         </StyledLine>
-        <StyledLine>・実務経験5年以上</StyledLine>
-        <StyledLine>・管理職として模範的な言動ができる</StyledLine>
-        <StyledLine>・他部署との連携が取れる</StyledLine>
-        <StyledLine>・JavaScript（ESNext）のコーディングスキル</StyledLine>
-        <StyledLine>・Node.jsの知識</StyledLine>
-        <StyledLine>・Vue.jsの知識</StyledLine>
-        <StyledLine>・Gitの知識</StyledLine>
-        <StyledLine>・Dockerの知識</StyledLine>
-        <StyledLine>・RDBMSの知識</StyledLine>
-        <StyledLine>・最新技術のキャッチアップ能力、技術選定能力</StyledLine>
-        <StyledLine>・開発スケジュールを調整できる</StyledLine>
-        <StyledLine>・ノウハウを共有できる</StyledLine>
-        <StyledLine>・コードレビューができる</StyledLine>
-        <StyledLine>・開発部の責任を持てる</StyledLine>
-        <br />
         <StyledLine>
-          <b>WANT（PMとして）</b>
+          ・<b>DB</b>: PostgreSQL
         </StyledLine>
-        <StyledLine>・PMとしての経験</StyledLine>
-        <StyledLine>・経営的視点</StyledLine>
-        <StyledLine>・社員の育成</StyledLine>
-        <StyledLine>・アジャイル開発の知識</StyledLine>
-        <StyledLine>・企画/顧客折衝～保守までの経験</StyledLine>
-        <StyledLine>・開発予算の管理</StyledLine>
-        <StyledLine>・プレイングマネージャー</StyledLine>
-        <StyledLine>・GCP、AWSの知識</StyledLine>
-        <StyledLine>・TypeScriptの知識</StyledLine>
-        <StyledLine>・Reactの知識</StyledLine>
-        <StyledLine>・Nuxt.jsの知識</StyledLine>
-        <StyledLine>・Kubernetesの知識</StyledLine>
-      </DefinitionBlock.Body>
-    </DefinitionBlock>
-    <DefinitionBlock>
-      <DefinitionBlock.Header>勤務時間</DefinitionBlock.Header>
-      <DefinitionBlock.Body>
-        <StyledLine>9:00 ～ 18:00（休憩1h）</StyledLine>
         <StyledLine>
-          ※試用期間満了後に、時差出勤勤務を行っても業務に支障がないと会社が指定した者については、8:00
-          ～ 10:00の間で時差出勤が可能（15分単位）
+          ・<b>コミュニケーション</b>: Slack
+        </StyledLine>
+        <StyledLine>
+          ・<b>開発ツール</b>: Git
+        </StyledLine>
+        <StyledLine>
+          ・<b>インフラ</b>: GitHub, GCP(GKE, Storage, Container Registry, DNS,
+          SQL, Memorystore, Functions, Scheduler), Kubernetes
         </StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
-      <DefinitionBlock.Header>諸手当</DefinitionBlock.Header>
+      <DefinitionBlock.Header>
+        開発環境（使用頻度としては若干下がるが使われている技術）
+      </DefinitionBlock.Header>
       <DefinitionBlock.Body>
-        <StyledLine>交通費全額支給（月額上限5万円）</StyledLine>
+        <StyledLine>
+          ・<b>言語</b>: HTML5, CSS3(SCSS, PostCSS)
+        </StyledLine>
+        <StyledLine>
+          ・<b>フレームワーク</b>: React(class-based, hooks), Storybook,
+          GatsbyJS, Webpack
+        </StyledLine>
+        <StyledLine>
+          ・<b>DB</b>: Redis
+        </StyledLine>
+        <StyledLine>
+          ・<b>クライアント</b>: Electron
+        </StyledLine>
+        <StyledLine>
+          ・<b>インフラ</b>: Netlify, GitHub(Pages, Actions, Packages), Algolia,
+          Mailgun, Cloudinary
+        </StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
-      <DefinitionBlock.Header>給与</DefinitionBlock.Header>
+      <DefinitionBlock.Header>雇用形態</DefinitionBlock.Header>
+      <DefinitionBlock.Body>
+        <StyledLine>・正社員</StyledLine>
+        <StyledLine>
+          ・試用期間：3か月（試用期間終了後、時差出勤可能）
+        </StyledLine>
+      </DefinitionBlock.Body>
+    </DefinitionBlock>
+    <DefinitionBlock>
+      <DefinitionBlock.Header>参考給与</DefinitionBlock.Header>
       <DefinitionBlock.Body>
         <StyledLine>
-          年収500万 ～ 800万程度（経験、能力、お任せする職責によって変動）
+          ・年収420万 ～ 620万程度（賞与込み、残業代は別途全額支給）向かいます
+          ※評価制度参照
         </StyledLine>
-        <StyledLine>※年収を12分した額が月給（月給41万 ～ 66万）</StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
       <DefinitionBlock.Header>給与の見直し</DefinitionBlock.Header>
       <DefinitionBlock.Body>
-        <StyledLine>年2回（4月及び10月）</StyledLine>
+        <StyledLine>・年2回（4月及び10月） ※評価制度参照</StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
-      <DefinitionBlock.Header>賞与</DefinitionBlock.Header>
+      <DefinitionBlock.Header>評価制度</DefinitionBlock.Header>
       <DefinitionBlock.Body>
-        <StyledLine>原則なし（年収に含まれているため）</StyledLine>
         <StyledLine>
-          ※ただし、会社の業績に応じて決算賞与を支給することがあり
+          エイミーでは「採用時は転職相場（市場価値）で給与が決定するのに、なぜ多くの企業は入社したら会社内の評価だけでその後の給与が決まるのか？」という疑問から、市場価値に着目した人事考課制度を実施しています。
         </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          ・市場価値を基準に、社内価値（社内においての評価）を加味して半年間の報酬を決定
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          <b>市場価値</b>
+        </StyledLine>
+        <StyledLine>
+          エイミーでは、今後の未来において「社員自身がキャリアの選択肢を持つ」ことがより重要になってくると考えています。そのため、給与見直しのタイミング（4月、10月）に外部機関に市場価値の算出を依頼し、その結果（高く評価されたスキルや、市場価値を高めるアドバイスなども含む）を半年間の給与の基準値としています。
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          <b>社内価値（3Arrival + 4Drive）</b>
+        </StyledLine>
+        <StyledLine>
+          企業理念「みんなが輝く多様な未来を創る」という理想を実現するために大切な価値観（3Arrival
+          + 4Drive）を、社内価値の評価基準としています。
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          <b>3Arrival：理想への道を築き上げる力</b>
+        </StyledLine>
+        <StyledLine>
+          挑戦（ベンチャーマインド）を忘れず、困難を恐れず、理想の実現に向けて一丸となって進むためスキル
+        </StyledLine>
+        <StyledLine>
+          1. 視座/視野（View） 私は、+1 の想像力を働かせる
+        </StyledLine>
+        <StyledLine>
+          2. 覚悟（Commit） 私は、現在の試練から視線を逸らさない
+        </StyledLine>
+        <StyledLine>
+          3. 未来志向/前向き（Future） 私は、過去に捉われず、未来に視線を向ける
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          <b>4Drive：築いた道を突き進む力</b>
+        </StyledLine>
+        <StyledLine>
+          3Arrivalで見据えた道を突き進むために必要な課題をクリアするためのスキル
+        </StyledLine>
+        <StyledLine>
+          1. 率先（Initiative） 私は、自ら先手を打ち、信頼を得る
+        </StyledLine>
+        <StyledLine>
+          2. 速い/早い（Fast） 私は、スピード対応を徹底する
+        </StyledLine>
+        <StyledLine>
+          3. 細部へのこだわり（Details） 私は、一字一句、誇りをもって仕上げる
+        </StyledLine>
+        <StyledLine>
+          4. お互いさま（Fellowship） 私は、チームは一人ではないことを知っている
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          この3Arrivalと4Driveは、私たちが「こんな人と一緒に働きたい」という指標であるとともに、企業文化として大切にしている共通の価値観です。
+        </StyledLine>
+      </DefinitionBlock.Body>
+    </DefinitionBlock>
+    <DefinitionBlock>
+      <DefinitionBlock.Header>所定勤務時間</DefinitionBlock.Header>
+      <DefinitionBlock.Body>
+        <StyledLine>・8H（9:00～18:00）</StyledLine>
+        <StyledLine>・時差出勤対象社員は、8:00～10:00の間で出勤可能</StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
       <DefinitionBlock.Header>休日休暇</DefinitionBlock.Header>
       <DefinitionBlock.Body>
-        <StyledLine>完全週休2日制（土日）</StyledLine>
-        <StyledLine>国民の祝日</StyledLine>
-        <StyledLine>GW</StyledLine>
-        <StyledLine>夏季休暇</StyledLine>
-        <StyledLine>年末年始休暇</StyledLine>
-        <StyledLine>※年間休日125日</StyledLine>
-        <br />
-        <StyledLine>年次有給休暇</StyledLine>
-        <StyledLine>※一時間単位の取得可能（年間最大40h）</StyledLine>
         <StyledLine>
+          ・完全週休2日制（土日）、国民の祝日、GW休暇、夏季休暇、年末年始休暇
+          <br />
+          ※年間休日125日
+        </StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>
+          ・年次有給休暇
+          <br />
+          ※一時間単位の取得可能（年間最大40h）
+          <br />
           ※入社時に最大3日の前借りが可能（入社時に3日付与した場合、入社半年後に7日付与）
         </StyledLine>
-        <br />
-        <StyledLine>慶弔休暇</StyledLine>
+        <StyledLine>
+          <br />
+        </StyledLine>
+        <StyledLine>・慶弔休暇</StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
       <DefinitionBlock.Header>保険</DefinitionBlock.Header>
       <DefinitionBlock.Body>
-        <StyledLine>社会保険完備</StyledLine>
+        <StyledLine>
+          ・各種社会保険完備（雇用・労災・健康・厚生年金）
+        </StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
       <DefinitionBlock.Header>福利厚生</DefinitionBlock.Header>
       <DefinitionBlock.Body>
-        <StyledLine>私服勤務</StyledLine>
-        <StyledLine>健康診断</StyledLine>
+        <StyledLine>・定期健康診断：年一回</StyledLine>
+        <StyledLine>・私服勤務可</StyledLine>
+        <StyledLine>・光熱費補助：5,000円（テレワーク社員のみ）</StyledLine>
+        <StyledLine>
+          ・交通費全額支給：最大50,000円（事務所勤務社員のみ）
+        </StyledLine>
+        <StyledLine>・時差出勤</StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
     <DefinitionBlock>
-      <DefinitionBlock.Header>応募方法</DefinitionBlock.Header>
+      <DefinitionBlock.Header>受動喫煙対策に関する表示</DefinitionBlock.Header>
       <DefinitionBlock.Body>
         <StyledLine>
-          履歴書(写真貼付・メールアドレス・志望動機必須)を弊社宛にご郵送ください。書類選考の上、合否に関わらずご連絡いたします。
+          ・事務所内完全禁煙（事務所所在地ビル内に喫煙所あり（現在新型コロナウィルス感染予防対策として閉鎖中
+        </StyledLine>
+      </DefinitionBlock.Body>
+    </DefinitionBlock>
+    <DefinitionBlock>
+      <DefinitionBlock.Header>選考フロー</DefinitionBlock.Header>
+      <DefinitionBlock.Body>
+        <StyledLine>
+          1.
+          メールにて履歴書(写真貼付・志望動機必須)、（職務経歴がある場合は）職務経歴書を送付
+        </StyledLine>
+        <StyledLine>2. 書類選考</StyledLine>
+        <StyledLine>3. 思考の傾向性を測るアセスメント実施</StyledLine>
+        <StyledLine>4. 面接（2-3回)</StyledLine>
+        <StyledLine>5. 社外価値算出（1次面接後）</StyledLine>
+        <StyledLine>6. 内定</StyledLine>
+      </DefinitionBlock.Body>
+    </DefinitionBlock>
+    <DefinitionBlock>
+      <DefinitionBlock.Header>
+        思考の傾向性を測るアセスメントについて
+      </DefinitionBlock.Header>
+      <DefinitionBlock.Body>
+        <StyledLine>
+          ・このアセスメントは思考の傾向を事前に把握することで人的なトラブルやストレスを回避し、働き易い職場を形成することを目的として実施しており、採用の合否には影響ありません。
+        </StyledLine>
+      </DefinitionBlock.Body>
+    </DefinitionBlock>
+    <DefinitionBlock>
+      <DefinitionBlock.Header>特記事項</DefinitionBlock.Header>
+      <DefinitionBlock.Body>
+        <StyledLine>
+          ・新型コロナウィルス感染予防対策として、原則テレワーク勤務となっております。
+        </StyledLine>
+      </DefinitionBlock.Body>
+    </DefinitionBlock>
+    <DefinitionBlock>
+      <DefinitionBlock.Header>応募先</DefinitionBlock.Header>
+      <DefinitionBlock.Body>
+        <StyledLine>
+          ・必要書類を添付の上、メール（recruit@eimee.co.jp）にてご応募ください。
+        </StyledLine>
+        <StyledLine>
+          ・書類選考の上、合否に関わらずご連絡いたします。
         </StyledLine>
       </DefinitionBlock.Body>
     </DefinitionBlock>
   </DefinitionCard>
 );
 
-export default RecruitCareerSummary;
+export default RecruitSystemEngineerSummary;
